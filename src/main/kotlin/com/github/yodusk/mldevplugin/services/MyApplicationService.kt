2f82e9892b0428fe832131169e492bf798e0d0c4
GitHub Action <action@github.com>
2022-06-25 21:10:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/yodusk/mldevplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/yodusk/mldevplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.yodusk.mldevplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.yodusk.mldevplugin.MyBundle
 
 class MyApplicationService {
 
